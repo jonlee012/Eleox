1a21f1641f5f794c2a3a439916d649c2ab2d5193
jonlee012 <jonlee86.jl@gmail.com>
2022-09-20 14:02:25 -0500

    changes to seeder

--- seeder.sql
+++ seeder.sql
@@ -2,15 +2,17 @@ CREATE DATABASE IF NOT EXISTS eleox_db;
 
 USE eleox_db;
 
-CREATE TABLE IF NOT EXISTS users (
-                           id bigint NOT NULL,
-                           email varchar(255),
-                           password varchar(255),
-                           username varchar(255),
-                           PRIMARY KEY (ID)
+# CREATE TABLE IF NOT EXISTS users (
+#                            id bigint NOT NULL,
+#                            email varchar(255),
+#                            password varchar(255),
+#                            username varchar(255),
+#                            PRIMARY KEY (ID)
+#
+# );
+#
+# INSERT INTO users (email, password, username)
+# VALUES ('interview@eleox.com','eleox', 'interview'),
+#        ('jon@eleox.com','test', 'jon');
 
-);
 
-INSERT INTO users (email, password, username)
-VALUES ('interview@eleox.com','eleox', 'interview'),
-       ('jon@eleox.com','test', 'jon');
